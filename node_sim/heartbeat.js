d4efa14343dad2e037c22fc5776283dec5fc30ea
Rhushya <rhushya2004@gmail.com>
2025-04-07 13:29:12 +0530

    heatbeat.js file upadted

---
+++ node_sim/heartbeat.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+const API_SERVER = 'http://host.docker.internal:3000'; // or your host machine IP
+
+function startHeartbeat(nodeId) {
+  setInterval(async () => {
+    try {
+      await axios.post(${API_SERVER}/heartbeat, { nodeId });
+      console.log(Heartbeat sent from ${nodeId});
+    } catch (err) {
+      console.error('Heartbeat failed:', err.message);
+    }
+  }, 5000); // every 5 seconds
+}
+
+module.exports = { startHeartbeat };
+
